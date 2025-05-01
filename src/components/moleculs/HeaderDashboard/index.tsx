031f7d60e8427a2b78735197f9c6f352b5f50a9d
Nav192 <kurniawangosal2429@gmail.com>
2025-05-01 17:20:43 +0800

    header dashboard

---
+++ src/components/moleculs/HeaderDashboard/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import {View, Text, StyleSheet, Image} from 'react-native';
+import {UserIcon} from 'lucide-react-native';
+
+const HeaderDashboard = ({name, email, welcome, photo}) => {
+  return (
+    <View style={styles.header}>
+      <View style={styles.userInfo}>
+        {photo ? (
+          <Image source={{uri: photo}} style={styles.profileImage} />
+        ) : (
+          <UserIcon size={40} />
+        )}
+        <View style={{marginLeft: 10}}>
+          <Text style={styles.name}>{name}</Text>
+          <Text style={styles.email}>{email}</Text>
+        </View>
+      </View>
+      <Text style={styles.welcome}>{welcome}</Text>
+    </View>
+  );
+};
+
+export default HeaderDashboard;
