ae738036fe7f1e89d4c61e05e2e46a133a93ae2e
joshua <joshuawowiling456@gmail.com>
2025-05-01 14:58:11 +0700

    useeffecet

--- src/pages/SplashScreen/index.tsx
+++ src/pages/SplashScreen/index.tsx
@@ -1,5 +1,6 @@
 import {StyleSheet, View, Image} from 'react-native';
-import React from 'react';
+
+import React, {useEffect} from 'react';
 import {Logo} from '../../assets';
 
 const SplashScreen = () => {
