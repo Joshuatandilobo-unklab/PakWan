178c059ce21061ac10d746f868a7dc027144d73e
joshua <joshuawowiling456@gmail.com>
2025-05-01 14:39:01 +0700

    pages sign up

--- App.tsx
+++ App.tsx
@@ -2,9 +2,10 @@ import CheckoutPage from './src/pages/CheckOut';
 import PaymentSuccess from './src/pages/payment';
 import PaymentFailed from './src/pages/paymentx';
 import SplashScreen from './src/pages/SplashScreen';
+import SignUp from './src/pages/SignUp';
 
 const App = () => {
-  return <SplashScreen />;
+  return <SignUp />;
 };
 
 export default App;
--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -6,10 +6,101 @@ import {
   TouchableOpacity,
   Image,
 } from 'react-native';
-
 import Header from '../../components/moleculs/Header';
 import {AddPhoto} from '../../assets';
 import TextInput from '../../components/moleculs/TextInput';
 import Button from '../../components/atoms/Button';
 import Gap from '../../components/atoms/Gap';
 import BackButton from '../../components/atoms/BackButton';
+
+const SignUp = () => {
+  return (
+    <View style={styles.pageContainer}>
+      <ScrollView>
+        <BackButton />
+        <Header title="Sign Up" />
+        <View style={styles.contentContainer}>
+          <View style={styles.profileContainer}>
+            <View style={styles.profile}>
+              <View style={styles.addPhoto}>
+                <TouchableOpacity style={styles.profile} activeOpacity={0.7}>
+                  <Image source={AddPhoto} style={styles.avatar} />
+                </TouchableOpacity>
+              </View>
+            </View>
+          </View>
+
+          <TextInput label="Full Name" placeholder="Type your full name" />
+          <Gap height={15} />
+
+          <TextInput
+            label="Email Address"
+            placeholder="Type your email address"
+          />
+          <Gap height={15} />
+
+          <TextInput
+            label="Password"
+            placeholder="Type your password"
+            secureTextEntry
+            withIcon
+          />
+          <Gap height={15} />
+
+          <TextInput
+            label="Confirm Password"
+            placeholder="Type again to confirm your password"
+            secureTextEntry
+            withIcon
+          />
+          <Gap height={5} />
+
+          <Button
+            label="Continue"
+            color="#328E6E"
+            textColor="#FFFFFF"
+            onPress={() => {}}
+          />
+        </View>
+      </ScrollView>
+    </View>
+  );
+};
+
+export default SignUp;
+
+const styles = StyleSheet.create({
+  pageContainer: {
+    flex: 1,
+    backgroundColor: '#E1EEBC',
+  },
+  contentContainer: {
+    marginTop: 25,
+    flex: 1,
+    paddingHorizontal: 24,
+  },
+  profileContainer: {
+    alignItems: 'center',
+    marginBottom: 15,
+  },
+  profile: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: 100,
+    width: 100,
+    borderRadius: 50,
+    overflow: 'hidden',
+  },
+  addPhoto: {
+    width: 80,
+    height: 80,
+    borderRadius: 40,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  avatar: {
+    height: '100%',
+    width: '100%',
+    resizeMode: 'cover',
+  },
+});
