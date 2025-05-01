f28618bd1aaa20d5fcd20a9fdb4895493baec424
Draisen2711 <draisenskilrey@gmail.com>
2025-05-01 11:53:50 +0800

    ..

--- App.tsx
+++ App.tsx
@@ -1,8 +1,9 @@
+import CheckoutPage from './src/pages/CheckOut';
 import PaymentSuccess from './src/pages/payment';
 import PaymentFailed from './src/pages/paymentx';
 
 const App = () => {
-  return <PaymentFailed />;
+  return <CheckoutPage />;
 };
 
 export default App;
