52b74beafeb93470c7cb51d491dc804ca97b08af
Nav192 <kurniawangosal2429@gmail.com>
2025-05-01 17:43:32 +0800

    profile picture

---
+++ src/components/moleculs/ProfilePicture/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import {View, TouchableOpacity, Text, StyleSheet, Image} from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+const ProfilePicture = ({photoUri, onPress}) => {
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity style={styles.uploadButton} onPress={onPress}>
+        {photoUri ? (
+          <Image source={{uri: photoUri}} style={styles.photo} />
+        ) : (
+          <>
+            <Icon name="add-a-photo" size={32} color="#888" />
+            <Text style={styles.uploadText}>Add Photo</Text>
+          </>
+        )}
+      </TouchableOpacity>
+    </View>
+  );
+};
