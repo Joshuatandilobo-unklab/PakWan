7dda62cac5e52c384b7b47e4056d23ec802c4bd6
Nav192 <kurniawangosal2429@gmail.com>
2025-05-01 16:54:55 +0800

    commit App.tsx

--- App.tsx
+++ App.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import CheckoutPage from './src/pages/CheckOut';
 import PaymentSuccess from './src/pages/payment';
 import PaymentFailed from './src/pages/paymentx';
@@ -8,8 +9,25 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import FlashMessage from 'react-native-flash-message';
 
+const Stack = createNativeStackNavigator();
+
 const App = () => {
-  return <SignUp />;
+  return (
+    <ProductProvider>
+      <CartProvider>
+        <NavigationContainer>
+          <Stack.Navigator>
+            <Stack.Screen
+              name="SplashScreen"
+              component={SplashScreen}
+              options={{headerShown: false}}
+            />
+          </Stack.Navigator>
+          <FlashMessage position="top" />
+        </NavigationContainer>
+      </CartProvider>
+    </ProductProvider>
+  );
 };
 
 export default App;
