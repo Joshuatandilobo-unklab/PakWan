2fbc8bdc9ec0853b22dbce09be788f46e62f8cff
joshua <joshuawowiling456@gmail.com>
2025-05-01 15:02:57 +0700

    onsubmit

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -10,4 +10,19 @@ import BackButton from '../../components/atoms/BackButton';
 const SignIn = ({navigation}) => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
-  
+
+
+    const onSubmit = () => {
+        const auth = getAuth();
+        signInWithEmailAndPassword(auth, email, password)
+          .then(userCredential => {
+            const user = userCredential.user;
+            navigation.navigate('Dashboard', {uid: user.uid});
+          })
+          .catch(error => {
+            showMessage({
+              message: 'Incorrect Email or Password',
+              type: 'danger',
+            });
+          });
+      };
