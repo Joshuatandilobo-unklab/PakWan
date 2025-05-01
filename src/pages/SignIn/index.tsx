d272ca701311f82937ad22eaadd5fc476782f5bb
joshua <joshuawowiling456@gmail.com>
2025-05-01 15:02:17 +0700

    function signin

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -5,3 +5,9 @@ import TextInput from '../../components/moleculs/TextInput';
 import Button from '../../components/atoms/Button';
 import Gap from '../../components/atoms/Gap';
 import BackButton from '../../components/atoms/BackButton';
+
+
+const SignIn = ({navigation}) => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+  
