c7327cf15179b9f9fac4082feaf46cb7390ed538
Nav192 <kurniawangosal2429@gmail.com>
2025-05-01 17:12:57 +0800

    commit bottom navigator final

--- src/components/moleculs/BottomNavigator/BottomNavigator.tsx
+++ src/components/moleculs/BottomNavigator/BottomNavigator.tsx
@@ -34,3 +34,27 @@ const BottomNavigator = ({navigation}) => {
 };
 
 export default BottomNavigator;
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    paddingVertical: 10,
+    backgroundColor: '#E1EEBC',
+    borderTopLeftRadius: 15,
+    borderTopRightRadius: 15,
+    height: 70,
+    elevation: 5,
+  },
+  menuItem: {
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  label: {
+    fontFamily: 'Inder-Regular',
+    fontSize: 15,
+    marginTop: 4,
+    color: '#000',
+    textAlign: 'center',
+  },
+});
