436dbb18b24f1658946ae25a9cf1e5560ef1a5ab
Nav192 <kurniawangosal2429@gmail.com>
2025-05-01 17:24:29 +0800

    menu item

---
+++ src/components/moleculs/MenuItem/MenuItem.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {View, Text, TouchableOpacity, StyleSheet} from 'react-native';
+import {ShoppingBag, ShoppingCart} from 'lucide-react-native';
+
+const MenuItem = ({icon, title, onPress}) => {
+  const Icon = icon === 'bag' ? ShoppingBag : ShoppingCart;
+  return (
+    <TouchableOpacity style={styles.container} onPress={onPress}>
+      <Icon size={40} />
+      <Text style={styles.label}>{title}</Text>
+    </TouchableOpacity>
+  );
+};
+
+export default MenuItem;
