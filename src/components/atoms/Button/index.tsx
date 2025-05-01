241289caabacc57782c6bfff0822cab3b9dd9de8
joshua <joshuawowiling456@gmail.com>
2025-05-01 15:08:38 +0700

    button

--- src/components/atoms/Button/index.tsx
+++ src/components/atoms/Button/index.tsx
@@ -1,33 +1,36 @@
+import {StyleSheet, Text, View, TouchableOpacity} from 'react-native';
 import React from 'react';
-import {TouchableOpacity, Text, StyleSheet} from 'react-native';
 
-interface ButtonProps {
-  title: string;
-  onPress: () => void;
-}
-
-const Button: React.FC<ButtonProps> = ({title, onPress}) => {
+const Button = ({
+  label,
+  color = '#02CF8E',
+  textColor = '#020202',
+  onPress,
+  style,
+}) => {
   return (
-    <TouchableOpacity style={styles.button} onPress={onPress}>
-      <Text style={styles.text}>{title}</Text>
+    <TouchableOpacity
+      style={[styles.button(color), style]}
+      activeOpacity={0.5}
+      onPress={onPress}>
+      <Text style={styles.label(textColor)}>{label}</Text>
     </TouchableOpacity>
   );
 };
 
-const styles = StyleSheet.create({
-  button: {
-    backgroundColor: '#EC221F',
-    paddingVertical: 12,
-    paddingHorizontal: 24,
-    borderRadius: 8,
-    alignItems: 'center',
-    marginTop: 100,
-    width: 362,
-  },
-  text: {
-    color: '#fff',
-    fontSize: 16,
-  },
-});
-
 export default Button;
+
+const styles = StyleSheet.create({
+  button: color => ({
+    backgroundColor: color,
+    borderRadius: 8,
+    paddingVertical: 12,
+    marginTop: 20,
+  }),
+  label: textColor => ({
+    textAlign: 'center',
+    fontFamily: 'Poppins-Medium',
+    fontSize: 14,
+    color: textColor,
+  }),
+});
