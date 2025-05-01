fdbbba6b4fc7ff25d45e244af4d552ec70f62ebb
joshua <joshuawowiling456@gmail.com>
2025-05-01 14:59:31 +0700

    navigation

--- src/pages/SplashScreen/index.tsx
+++ src/pages/SplashScreen/index.tsx
@@ -3,7 +3,10 @@ import {StyleSheet, View, Image} from 'react-native';
 import React, {useEffect} from 'react';
 import {Logo} from '../../assets';
 
-const SplashScreen = () => {
+const SplashScreen = ({navigation}) => {
+  useEffect(() => {
+    setTimeout(() => navigation.replace('SignIn'), 3000);
+  }, []);
   return (
     <View style={styles.container}>
       <Image source={Logo} />
