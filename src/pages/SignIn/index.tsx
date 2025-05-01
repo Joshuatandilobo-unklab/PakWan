b698c929ecd6e4408d01dfbe694ea2967c444cc7
joshua <joshuawowiling456@gmail.com>
2025-05-01 15:00:41 +0700

    import react

---
+++ src/pages/SignIn/index.tsx
@@ -0,0 +1,7 @@
+import {StyleSheet, Text, View, TouchableOpacity} from 'react-native';
+import React, {useState} from 'react';
+import Header from '../../components/moleculs/Header';
+import TextInput from '../../components/moleculs/TextInput';
+import Button from '../../components/atoms/Button';
+import Gap from '../../components/atoms/Gap';
+import BackButton from '../../components/atoms/BackButton';
