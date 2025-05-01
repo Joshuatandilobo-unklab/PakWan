5cc27bd22d3e848b35deadcfddc950130491e658
Nav192 <kurniawangosal2429@gmail.com>
2025-05-01 17:04:44 +0800

    commit App.tsx 2

--- App.tsx
+++ App.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import CheckoutPage from './src/pages/CheckOut';
-import PaymentSuccess from './src/pages/payment';
-import PaymentFailed from './src/pages/paymentx';
+import PaymentSuccess from './src/pages/PaymentSuccess';
+import PaymentFailed from './src/pages/PaymentFailed';
 import SplashScreen from './src/pages/SplashScreen';
 import SignUp from './src/pages/SignUp';
+import SignIn from './src/pages/SignIn';
+import Dashboard from './src/pages/Dashboard';
+import Product from './src/pages/Product';
+import Cart from './src/pages/Cart';
+import ProductDetail from './src/pages/ProductDetail';
+import ProfilePage from './src/pages/Profile';
+import ProfileSettings from './src/pages/ProfileSettings';
 
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
@@ -20,6 +27,61 @@ const App = () => {
             <Stack.Screen
               name="SplashScreen"
               component={SplashScreen}
+              options={{headerShown: false}}
+            />
+            <Stack.Screen
+              name="SignIn"
+              component={SignIn}
+              options={{headerShown: false}}
+            />
+            <Stack.Screen
+              name="SignUp"
+              component={SignUp}
+              options={{headerShown: false}}
+            />
+            <Stack.Screen
+              name="Dashboard"
+              component={Dashboard}
+              options={{headerShown: false}}
+            />
+            <Stack.Screen
+              name="Product"
+              component={Product}
+              options={{headerShown: false}}
+            />
+            <Stack.Screen
+              name="Cart"
+              component={Cart}
+              options={{headerShown: false}}
+            />
+            <Stack.Screen
+              name="ProductDetail"
+              component={ProductDetail}
+              options={{headerShown: false}}
+            />
+            <Stack.Screen
+              name="Profile"
+              component={ProfilePage}
+              options={{headerShown: false}}
+            />
+            <Stack.Screen
+              name="ProfileSettings"
+              component={ProfileSettings}
+              options={{headerShown: false}}
+            />
+            <Stack.Screen
+              name="Checkout"
+              component={CheckoutPage}
+              options={{headerShown: false}}
+            />
+            <Stack.Screen
+              name="PaymentSuccess"
+              component={PaymentSuccess}
+              options={{headerShown: false}}
+            />
+            <Stack.Screen
+              name="PaymentFailed"
+              component={PaymentFailed}
               options={{headerShown: false}}
             />
           </Stack.Navigator>
