37c053c4d76ab8d1a5cd42b52e02765ed60555ba
joshua <joshuawowiling456@gmail.com>
2025-05-01 14:00:42 +0700

    Header

---
+++ src/pages/SignUp/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {
+  View,
+  StyleSheet,
+  ScrollView,
+  TouchableOpacity,
+  Image,
+} from 'react-native';
+
+import Header from '../../components/moleculs/Header';
+import {AddPhoto} from '../../assets';
+import TextInput from '../../components/moleculs/TextInput';
+import Button from '../../components/atoms/Button';
+import Gap from '../../components/atoms/Gap';
+import BackButton from '../../components/atoms/BackButton';
