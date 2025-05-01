8c01aeaad0106cf4541f018cf147a7101e87ffce
joshua <joshuawowiling456@gmail.com>
2025-05-01 13:35:36 +0700

    splashscreen

--- App.tsx
+++ App.tsx
@@ -1,9 +1,10 @@
 import CheckoutPage from './src/pages/CheckOut';
 import PaymentSuccess from './src/pages/payment';
 import PaymentFailed from './src/pages/paymentx';
+import SplashScreen from './src/pages/SplashScreen';
 
 const App = () => {
-  return <CheckoutPage />;
+  return <SplashScreen />;
 };
 
 export default App;
---
+++ src/pages/SplashScreen/index.tsx
@@ -0,0 +1,21 @@
+import {StyleSheet, View, Image} from 'react-native';
+import React from 'react';
+import {Logo} from '../../assets';
+
+const SplashScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Image source={Logo} />
+    </View>
+  );
+};
+export default SplashScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#E1EEBC',
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
